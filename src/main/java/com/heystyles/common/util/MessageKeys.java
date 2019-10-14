1a8b4222b71e206bc424c201737887a50b81726d
pedrolop90 <pedrolop90@hotmail.com>
2019-10-14 02:07:12 -0500

    mejoras en MessageKeys....

--- src/main/java/com/heystyles/common/util/MessageKeys.java
+++ src/main/java/com/heystyles/common/util/MessageKeys.java
@@ -1,14 +1,14 @@
 package com.heystyles.common.util;
 
 public interface MessageKeys {
-    String NOT_FOUND = "jamestown.commons.not.found";
-    String NOT_FOUND_OBJECT = "jamestown.commons.not.found.object";
-    String VALIDATION_MESSAGE = "jamestown.commons.validation.message.default";
-    String VALIDATION_INVALID_JSON = "jamestown.commons.validation.json.invalid";
-    String VALIDATION_ENUM_INVALID = "jamestown.commons.validation.enum.invalid";
-    String ERROR_DB_CONNECTION = "jamestown.commons.error.db.connection";
-    String ERROR_DB_CONSTRAINTS = "jamestown.commons.error.db.constraints";
-    String ERROR_UNEXPECTED = "jamestown.commons.error.unexpected";
-    String ERROR_CONVERTER_NOT_FOUND = "jamestown.commons.error.converter.not-found";
+    String NOT_FOUND = "heystyles.commons.not.found";
+    String NOT_FOUND_OBJECT = "heystyles.commons.not.found.object";
+    String VALIDATION_MESSAGE = "heystyles.commons.validation.message.default";
+    String VALIDATION_INVALID_JSON = "heystyles.commons.validation.json.invalid";
+    String VALIDATION_ENUM_INVALID = "heystyles.commons.validation.enum.invalid";
+    String ERROR_DB_CONNECTION = "heystyles.commons.error.db.connection";
+    String ERROR_DB_CONSTRAINTS = "heystyles.commons.error.db.constraints";
+    String ERROR_UNEXPECTED = "heystyles.commons.error.unexpected";
+    String ERROR_CONVERTER_NOT_FOUND = "heystyles.commons.error.converter.not-found";
 }
 
